ebde17218fa9962e58dabbd5891f96faafdb3600
menfengchao <549514557@qq.com>
2018-05-07 20:55:59 +0800

    登录调试通过

--- manager/manager-service/src/main/java/com/soa/mfc/service/impl/UserServiceImpl.java
+++ manager/manager-service/src/main/java/com/soa/mfc/service/impl/UserServiceImpl.java
@@ -1,19 +1,33 @@
 package com.soa.mfc.service.impl;
 
+import com.soa.mfc.mapper.UserMapper;
 import com.soa.mfc.pojo.User;
 import com.soa.mfc.service.UserService;
 import com.soa.mfc.common.pojo.MfcResult;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
+
+import javax.annotation.Resource;
 
 /**
- * Created by yunxin on 2018/5/3.
+ * Created by MFC on 2018/5/3.
+ * 用户相关服务
  */
 @Service
 public class UserServiceImpl implements UserService {
 
+    @Autowired
+    private UserMapper userMapper;
+
     @Override
-    public MfcResult addUser(User user) {
-       System.out.println(user.getNickname());
-        return new MfcResult("测试通过");
+    public MfcResult loginUser(User user) {
+        User use = userMapper.nameSelectPsaa(user);
+        if(!StringUtils.isEmpty(use) && use.getPswd().equals(user.getPswd())){
+            return new MfcResult(200,"SUCCESS","测试通过");
+        }else
+        {
+            return new MfcResult(200,"FAIL","测试通过登录失败");
+        }
     }
 }
